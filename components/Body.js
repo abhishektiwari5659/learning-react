133457cf804c1094d7fee33e01e94ab05bc9102c
HeheAbhi <abhishekti5659@gmail.com>
2025-09-14 20:44:11 +0530

    added search button

--- components/Body.js
+++ components/Body.js
@@ -1,70 +1,3 @@
-// import { useEffect, useState } from "react";
-// import ResCard from "./ResCard";
-// import ShimmerCard from "./ShimmerCard"
-
-// const Body = () => {
-//   const [restaurants, setRestaurants] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   const fetchData = async () => {
-//   try {
-//     const response = await fetch(
-//       `https://api.allorigins.win/raw?url=${encodeURIComponent(
-//         "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7041&lng=77.1025&page_type=DESKTOP_WEB_LISTING"
-//       )}`
-//     );
-
-//     const json = await response.json();
-//     console.log("Full API Response:", json); // 👀 check structure
-
-//     const cards = json?.data?.cards || [];
-
-//     const restaurantCard = cards.find(
-//       (c) => c?.card?.card?.gridElements?.infoWithStyle?.restaurants
-//     );
-
-//     const resList =
-//       restaurantCard?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
-
-//     setRestaurants(resList);
-//   } catch (err) {
-//     console.error("Error fetching restaurants:", err);
-//     setError("Failed to load restaurants. Please try again later.");
-//   } finally {
-//     setLoading(false);
-//   }
-// };
-
-//   return (
-//     <div className="res-con">
-//       {loading
-//         ? Array(8)
-//             .fill("")
-//             .map((_, i) => <ShimmerCard key={i} />)
-//         : restaurants.map((res) => (
-//             <ResCard
-//               key={res.info.id}
-//               name={res.info.name}
-//               cuisine={res.info.cuisines.join(", ")}
-//               image={
-//                 "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_400/" +
-//                 res.info.cloudinaryImageId
-//               }
-//               price={res.info.costForTwo}
-//               deliveryTime={res.info.sla?.slaString}
-//             />
-//           ))}
-//     </div>
-//   );
-// };
-
-// export default Body;
-
 
 import { useEffect, useState } from "react";
 import ResCard from "./ResCard";
@@ -126,7 +59,7 @@ const Body = () => {
   if (loading) {
     return (
       <div className="res-con">
-        {Array(8)
+        {Array(15)
           .fill("")
           .map((_, i) => (
             <ShimmerCard key={i} />
@@ -146,6 +79,7 @@ const Body = () => {
           placeholder="Search restaurants..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleSearch()}
           className="search-input"
         />
         <button onClick={handleSearch} className="search-button">
